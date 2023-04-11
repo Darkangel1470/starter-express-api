fa97d78dbbe92ccc19dcefc7b6bf365bb5428dea
nihar <navadkunder@gmail.com>
2023-04-11 10:39:45 +0530

    yellow

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('hellow world!')
+    
+    res.send('yellow!')
 })
 app.listen(process.env.PORT || 3000)
